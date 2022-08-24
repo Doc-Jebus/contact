1e516a08c01de55cd71c9acb3eb35847296c60e2
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-08-24 22:20:34 +0900

    Javascript solved issue with email confirmation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
-console.log("Hello World");
-    const email = document.querySelector('#email');
-    const emailError = document.querySelector('#email-error');
-    let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#email-error');
+const confirmation = document.querySelector('#confirmation');
+let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let notMatch = !validRegex;
 
-let confirmation = "✓";
-let emailWarning = "Please enter a valid email address!";
+let confirmCheck = "✓";
+let emailWarning = "Please enter a valid email address.";
+
 
 email.addEventListener('focusout', (e) => {
-    
-    if (e.target.value.match(validRegex)) {
-        emailError.textContent = confirmation;
-    } else {
-        emailError.textContent = emailWarning;
-    }
+  e.target.value.match(validRegex) ? 
+	confirmation.textContent = confirmCheck :
+	emailError.textContent = emailWarning;
 })
-    
+
+email.addEventListener('focusin', (e) => {
+  confirmation.textContent = '';
+	emailError.textContent = '';
+	return;
+})
+
+
