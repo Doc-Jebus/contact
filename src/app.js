3b132a458bf9d407e1933bea6261be7b1e6f6ea0
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-08-25 11:30:05 +0900

    Adding check to name input.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 const email = document.querySelector('#email');
 const emailError = document.querySelector('#email-error');
 const confirmation = document.querySelector('#confirmation');
+const firstName = document.querySelector('#first-name');
+const firstNameConfirm = document.querySelector('#first-name-confirm');
+const lastName = document.querySelector('#last-name');
 let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-let notMatch = !validRegex;
+
 
 let confirmCheck = "✓";
 let emailWarning = "Please enter a valid email address.";
 
 
-email.addEventListener('focusout', (e) => {
+email.addEventListener('focusout', (e)=> {
   e.target.value.match(validRegex) ? 
 	confirmation.textContent = confirmCheck :
 	emailError.textContent = emailWarning;
 })
 
-email.addEventListener('focusin', (e) => {
+email.addEventListener('focusin', ()=> {
   confirmation.textContent = '';
 	emailError.textContent = '';
 	return;
+})
+
+firstName.addEventListener('focusout', (e)=> {
+	let firstNam = document.getElementById('first-name').value;
+	let firstStr = firstNam.toString();
+	for(let i = 0; i < firstNam.length; i++) {
+		firstNam[i].length < 1 ?
+		firstNameConfirm.textContent = confirmCheck :
+		firstName.value = '';
+	}
 })
 
 
